2279532a1c43dc2ddaa1ef54a6df9e6c08ed1b8a
Abhishek Gupta <aiwithabhishek@gmail.com>
2025-09-29 19:24:04 +0530

    Implemented List all expense

--- app/routers/expenses.py
+++ app/routers/expenses.py
@@ -1,6 +1,7 @@
 from fastapi import Depends, APIRouter
 from sqlalchemy.orm import Session
 from app.utils.dependencies import get_db, get_current_user
+from app.models.expense import Expense
 
 router = APIRouter()
 
@@ -9,4 +10,15 @@ router = APIRouter()
 def list_expenses(
     db: Session = Depends(get_db), current_user: int = Depends(get_current_user)
 ):
-    return {"message": f"Expenses for user {current_user}"}
+    """Retrieve a list of all expenses for the current user."""
+    expenses = db.query(Expense).filter(Expense.user_id == current_user).all()
+    return [
+        {
+            "id": expense.id,
+            "amount": expense.amount,
+            "description": expense.description,
+            "category_id": expense.category_id,
+            "date": expense.date,
+        }
+        for expense in expenses
+    ]
--- app/utils/dependencies.py
+++ app/utils/dependencies.py
@@ -4,9 +4,7 @@ from app.database import SessionLocal
 from .auth import decode_access_token
 
 # The tokenUrl must match the full path of your login route
-print("HELLO FROM dependencies.py")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
-print("OAuth2PasswordBearer FROM CODE: ", oauth2_scheme)
 
 def get_current_user(token: str = Depends(oauth2_scheme)):
     payload = decode_access_token(token)
@@ -17,6 +15,7 @@ def get_current_user(token: str = Depends(oauth2_scheme)):
             headers={"WWW-Authenticate": "Bearer"},
         )
     return int(payload["sub"])  # this is the user_id
+
 
 def get_db():
     db = SessionLocal()
