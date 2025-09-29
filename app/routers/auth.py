ab525ad77134c96d9e372fa3296c3d718e5dc1ae
Abhishek Gupta <aiwithabhishek@gmail.com>
2025-09-29 18:31:27 +0530

    Fixed Bearer Authentication bug. Implemented form based validation

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, status, Form
 from sqlalchemy.orm import Session
 from app.utils.dependencies import get_db
 from app import schemas
@@ -12,10 +12,10 @@ router = APIRouter()
 @router.post("/signup", response_model=schemas.UserOut)
 def signup(user: schemas.UserCreate, db: Session = Depends(get_db)):
     existing = db.query(User).filter(User.email == user.email).first()
-    
+
     if existing:
         raise HTTPException(status_code=400, detail="Email already registered")
-    
+
     hashed_pw = auth.hash_password(user.password)
     db_user = User(username=user.username, email=user.email, password_hash=hashed_pw)
     db.add(db_user)
@@ -23,15 +23,18 @@ def signup(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db.refresh(db_user)
     return db_user
 
-# Login
-@router.post("/login", response_model=schemas.Token)
-def login(user: schemas.UserLogin, db: Session = Depends(get_db)):
-    db_user = db.query(User).filter(User.email == user.email).first()
 
-    if not db_user or not auth.verify_password(user.password, db_user.password_hash):
+# Login
+
+
+@router.post("/login", response_model=schemas.Token)
+def login(
+    username: str = Form(...), password: str = Form(...), db: Session = Depends(get_db)
+):
+    db_user = db.query(User).filter(User.username == username).first()
+    if not db_user or not auth.verify_password(password, db_user.password_hash):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials"
         )
-
     access_token = auth.create_access_token({"sub": str(db_user.id)})
     return {"access_token": access_token, "token_type": "bearer"}
--- app/utils/dependencies.py
+++ app/utils/dependencies.py
@@ -4,8 +4,9 @@ from app.database import SessionLocal
 from .auth import decode_access_token
 
 # The tokenUrl must match the full path of your login route
+print("HELLO FROM dependencies.py")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
-
+print("OAuth2PasswordBearer FROM CODE: ", oauth2_scheme)
 
 def get_current_user(token: str = Depends(oauth2_scheme)):
     payload = decode_access_token(token)
